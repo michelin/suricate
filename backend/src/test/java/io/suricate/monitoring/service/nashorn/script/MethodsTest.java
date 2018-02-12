8888cb0c1bd453c337118f3c3c983c79d23da463
Mat V <mvincent.it@gmail.com>
2018-02-12 17:54:53 +0100

    Fix nashorn methods unit test

--- backend/src/test/java/io/suricate/monitoring/service/nashorn/script/MethodsTest.java
+++ backend/src/test/java/io/suricate/monitoring/service/nashorn/script/MethodsTest.java
@@ -2,23 +2,30 @@ package io.suricate.monitoring.service.nashorn.script;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@SpringBootTest
 public class MethodsTest {
 
-//    @Test
-//    public void testAtobNull(){
-//        Assert.assertEquals(null, Methods.btoa(null));
-//    }
-//
-//    @Test
-//    public void testAtobEmpty(){
-//        Assert.assertEquals(null, Methods.btoa(""));
-//    }
-//
-//    @Test
-//    public void testAtob(){
-//        Assert.assertEquals("c2Rmc2ZkZg==", Methods.btoa("sdfsfdf"));
-//        Assert.assertEquals("YWZzZGZkZnFzZGYgcXNmIHQnKHQgdHJlenQgYWV0cnpldHpldHJ0", Methods.btoa("afsdfdfqsdf qsf t'(t trezt aetrzetzetrt"));
-//    }
+    @Test
+    public void testAtobNull(){
+        Assert.assertEquals(null, Methods.btoa(null));
+    }
+
+    @Test
+    public void testAtobEmpty(){
+        Assert.assertEquals(null, Methods.btoa(""));
+    }
+
+    @Test
+    public void testAtob(){
+        Assert.assertEquals("c2Rmc2ZkZg==", Methods.btoa("sdfsfdf"));
+        Assert.assertEquals("YWZzZGZkZnFzZGYgcXNmIHQnKHQgdHJlenQgYWV0cnpldHpldHJ0", Methods.btoa("afsdfdfqsdf qsf t'(t trezt aetrzetzetrt"));
+    }
 
 }
