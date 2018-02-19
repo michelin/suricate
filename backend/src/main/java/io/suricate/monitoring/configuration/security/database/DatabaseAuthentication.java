a59534c6ad6df38c1772ff790ed121117bd2121e
Axel Chesné <axel.chesnes_ext@michelin.com>
2018-02-19 14:20:50 +0100

    Correct bug with DB Auth

--- backend/src/main/java/io/suricate/monitoring/configuration/security/database/DatabaseAuthentication.java
+++ backend/src/main/java/io/suricate/monitoring/configuration/security/database/DatabaseAuthentication.java
@@ -2,13 +2,9 @@ package io.suricate.monitoring.configuration.security.database;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
 
 /**
  * Database Authentification configurations
@@ -16,19 +12,6 @@ import javax.sql.DataSource;
 @Configuration
 @ConditionalOnProperty(name = "application.authentication.provider", havingValue = "database")
 public class DatabaseAuthentication {
-
-    /** The data source retrieve from properties */
-    private final DataSource dataSource;
-
-    /**
-     * Constructor
-     *
-     * @param dataSource The data source bean
-     */
-    @Autowired
-    public DatabaseAuthentication(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
 
     /**
      * Configure the Authentication manager for the database
@@ -39,18 +22,8 @@ public class DatabaseAuthentication {
     public void configureDatabase(AuthenticationManagerBuilder auth) throws Exception {
         auth
             .jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(passwordEncoder())
-            .authoritiesByUsernameQuery("select u.username, r.name from user u, user_role ur, role r where ur.user_id = u.id and ur.role_id = r.id and u.username = ?")
-            .usersByUsernameQuery("select username, password as password,1 FROM user where username = ?");
-    }
-
-    /**
-     * Configure the password encoder
-     *
-     * @return The password encoder
-     */
-    private PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder(25);
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .authoritiesByUsernameQuery("select u.username, r.name from user u, user_role ur, role r where ur.user_id = u.id and ur.role_id = r.id and u.username = ?")
+                .usersByUsernameQuery("select username, password as password,1 FROM user where username = ?");
     }
 }
