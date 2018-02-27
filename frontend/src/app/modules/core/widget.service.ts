cb7e81c9659fec7cd47d15e95be9341469d47651
Axel Chesné <axel.chesnes_ext@michelin.com>
2018-02-27 17:35:55 +0100

    Refactor services

--- frontend/src/app/modules/core/widget.service.ts
+++ frontend/src/app/modules/core/widget.service.ts
@@ -16,11 +16,10 @@
 
 import { Injectable } from '@angular/core';
 import {AbstractHttpService} from '../../shared/services/abstract-http.service';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {Category} from '../../shared/model/dto/Category';
 import {Widget} from '../../shared/model/dto/Widget';
-import {ProjectWidget} from '../../shared/model/dto/ProjectWidget';
 
 @Injectable()
 export class WidgetService extends AbstractHttpService  {
@@ -30,22 +29,10 @@ export class WidgetService extends AbstractHttpService  {
   }
 
   getCategories(): Observable<Category[]> {
-    let headers = new HttpHeaders();
-    headers = headers.append('Content-Type', 'application/json');
-
-    return this.http
-        .get<Category[]>(`${AbstractHttpService.BASE_URL}/${AbstractHttpService.WIDGET_URL}/categories`, {headers: headers})
-        .map(response => AbstractHttpService.extractData(response))
-        .catch((error: any) => AbstractHttpService.handleErrorObservable(error));
+    return this.http.get<Category[]>(`${AbstractHttpService.BASE_URL}/${AbstractHttpService.WIDGET_URL}/categories`);
   }
 
   getWidgetsByCategoryId(categoryId: number): Observable<Widget[]> {
-    let headers = new HttpHeaders();
-    headers = headers.append('Content-Type', 'application/json');
-
-    return this.http
-        .get<Widget[]>(`${AbstractHttpService.BASE_URL}/${AbstractHttpService.WIDGET_URL}/category/${categoryId}`, {headers: headers})
-        .map(response => AbstractHttpService.extractData(response))
-        .catch((error: any) => AbstractHttpService.handleErrorObservable(error));
+    return this.http.get<Widget[]>(`${AbstractHttpService.BASE_URL}/${AbstractHttpService.WIDGET_URL}/category/${categoryId}`);
   }
 }
