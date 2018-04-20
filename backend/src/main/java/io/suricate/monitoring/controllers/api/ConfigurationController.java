6a9fc1ce67b5f3e97fb34e418cc7db4378df7c24
Axel Chesné <axel.chesnes_ext@michelin.com>
2018-04-20 16:37:57 +0200

    Issue #154 - Refactor configuration controller

--- backend/src/main/java/io/suricate/monitoring/controllers/api/ConfigurationController.java
+++ backend/src/main/java/io/suricate/monitoring/controllers/api/ConfigurationController.java
@@ -2,10 +2,14 @@ package io.suricate.monitoring.controllers.api;
 
 import io.suricate.monitoring.model.dto.ConfigurationDto;
 import io.suricate.monitoring.model.entity.Configuration;
+import io.suricate.monitoring.model.mapper.ConfigurationMapper;
 import io.suricate.monitoring.service.api.ConfigurationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,13 +36,21 @@ public class ConfigurationController {
     private final ConfigurationService configurationService;
 
     /**
+     * The configuration mapper for Domain/Dto tranformation
+     */
+    private final ConfigurationMapper configurationMapper;
+
+    /**
      * Constructor
      *
      * @param configurationService Inject the configuration service
+     * @param configurationMapper The configuration mapper
      */
     @Autowired
-    public ConfigurationController(final ConfigurationService configurationService) {
+    public ConfigurationController(final ConfigurationService configurationService,
+                                   final ConfigurationMapper configurationMapper) {
         this.configurationService = configurationService;
+        this.configurationMapper = configurationMapper;
     }
 
     /**
@@ -47,13 +59,22 @@ public class ConfigurationController {
      */
     @RequestMapping(method = RequestMethod.GET)
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public List<ConfigurationDto> getAll() {
+    public ResponseEntity<List<ConfigurationDto>> getAll() {
         Optional<List<Configuration>> configurations = configurationService.getAll();
 
         if(!configurations.isPresent()) {
             LOGGER.debug("No configurations found");
+
+            return ResponseEntity
+                .noContent()
+                .cacheControl(CacheControl.noCache())
+                .build();
         }
 
-        return configurationService.toDTO(configurations.get());
+        return ResponseEntity
+            .ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .cacheControl(CacheControl.noCache())
+            .body(configurationMapper.toConfigurationDtosDefault(configurations.get()));
     }
 }
