30507346f21db8fc5a43b2f2d18dc7ec449c4e8d
Axel Chesné <axel.chesnes_ext@michelin.com>
2018-04-23 14:27:22 +0200

    Issue #79 - Retrieve the list of widgets from backend

--- frontend/src/app/modules/widget/widget.service.ts
+++ frontend/src/app/modules/widget/widget.service.ts
@@ -1,17 +1,19 @@
 /*
- * Copyright 2012-2018 the original author or authors.
+ *  /*
+ *  * Copyright 2012-2018 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 import { Injectable } from '@angular/core';
@@ -25,6 +27,14 @@ import {Widget} from '../../shared/model/dto/Widget';
 export class WidgetService extends AbstractHttpService  {
 
   /**
+   * Base URL for widgets
+   *
+   * @type {string}
+   */
+  private static readonly WIDGETS_BASE_URL = `${AbstractHttpService.BASE_API_URL}/${AbstractHttpService.WIDGETS_URL}`;
+
+
+  /**
    * Constructor
    *
    * @param {HttpClient} httpClient The http client service
@@ -34,12 +44,21 @@ export class WidgetService extends AbstractHttpService  {
   }
 
   /**
+   * Get the list of widgets
+   *
+   * @returns {Observable<Widget[]>} The list of widgets as observable
+   */
+  getWidgets(): Observable<Widget[]> {
+    return this.httpClient.get<Widget[]>(`${WidgetService.WIDGETS_BASE_URL}`);
+  }
+
+  /**
    * Retrieve every categories
    *
    * @returns {Observable<Category[]>} The categories as observable
    */
   getCategories(): Observable<Category[]> {
-    return this.httpClient.get<Category[]>(`${AbstractHttpService.BASE_API_URL}/${AbstractHttpService.WIDGETS_URL}/categories`);
+    return this.httpClient.get<Category[]>(`${WidgetService.WIDGETS_BASE_URL}/categories`);
   }
 
   /**
@@ -49,6 +68,6 @@ export class WidgetService extends AbstractHttpService  {
    * @returns {Observable<Widget[]>} The widgets as observable
    */
   getWidgetsByCategoryId(categoryId: number): Observable<Widget[]> {
-    return this.httpClient.get<Widget[]>(`${AbstractHttpService.BASE_API_URL}/${AbstractHttpService.WIDGETS_URL}/category/${categoryId}`);
+    return this.httpClient.get<Widget[]>(`${WidgetService.WIDGETS_BASE_URL}/category/${categoryId}`);
   }
 }
