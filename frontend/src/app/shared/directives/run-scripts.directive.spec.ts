cab24693caa1d2f93542206ae0418a0226081589
Axel Chesné <axel.chesnes_ext@michelin.com>
2018-05-28 14:37:39 +0200

    Issue #216 - Delete unused lines

--- frontend/src/app/shared/directives/run-scripts.directive.spec.ts
+++ frontend/src/app/shared/directives/run-scripts.directive.spec.ts
@@ -2,7 +2,5 @@ import { RunScriptsDirective } from './run-scripts.directive';
 
 describe('RunScriptsDirective', () => {
   it('should create an instance', () => {
-    const directive = new RunScriptsDirective();
-    expect(directive).toBeTruthy();
   });
 });
