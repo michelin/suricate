69c1d9faa30fdfa67a7c1648ded781bd5fdd2109
Axel Chesné <axel.chesnes_ext@michelin.com>
2018-06-08 14:31:03 +0200

    Issue #94 - Refactor auth guard

--- frontend/src/app/shared/auth/guards/auth.guard.ts
+++ frontend/src/app/shared/auth/guards/auth.guard.ts
@@ -16,45 +16,53 @@
  *
  */
 
-/*
- *  /*
- *  * Copyright 2012-2018 the original author or authors.
- *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  * you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  *      http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
- *
- */
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {CanActivate, CanActivateChild, Router} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/operator/take';
-import 'rxjs/add/operator/map';
-import {AuthenticationService} from '../authentication.service';
+import {of} from 'rxjs/observable/of';
+import {TokenService} from '../token.service';
+import {AuthenticationService} from '../../../modules/authentication/authentication.service';
 
+/**
+ * Manage the right of the users
+ */
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
 
-  constructor(private router: Router, private authService: AuthenticationService) {
+  /**
+   * The constructor
+   *
+   * @param {Router} router The router
+   * @param {AuthenticationService} authenticationService
+   * @param {TokenService} tokenService The token service
+   */
+  constructor(private router: Router,
+              private authenticationService: AuthenticationService,
+              private tokenService: TokenService) {
   }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return this.authService.isLoggedIn()
-        .take(1)
-        .map((isLoggedIn: boolean) => {
-          if (!isLoggedIn) {
-            this.router.navigate(['/login']);
-            return false;
-          }
-          return true;
-        });
+  /**
+   * Manage user roles
+   *
+   * @returns {Observable<boolean>}
+   */
+  canActivate(): Observable<boolean> {
+    if (this.tokenService.token && !this.tokenService.isTokenExpired()) {
+      return of(true);
+    }
+
+    // not logged in so redirect to login page
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+
+    return of(false);
+  }
+
+  /**
+   * Activation for child routes
+   * @returns {Observable<boolean>}
+   */
+  canActivateChild(): Observable<boolean> {
+    return this.canActivate();
   }
 }
