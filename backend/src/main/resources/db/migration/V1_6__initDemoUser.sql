0b9be8e535f0a444aa5b8f9f75a1a6181baf9e12
Axel Chesné <axel.chesnes_ext@michelin.com>
2018-06-12 17:04:09 +0200

    Fix SQL Script

--- backend/src/main/resources/db/migration/V1_6__initDemoUser.sql
+++ backend/src/main/resources/db/migration/V1_6__initDemoUser.sql
@@ -1,5 +1,5 @@
 -- Demo data
-INSERT INTO `role` (id, name, description) VALUES(1, 'ROLE_ADMIN', 'Administrator Role');
-INSERT INTO `role` (id, name, description) VALUES(2, 'ROLE_USER', 'User role');
-INSERT INTO `user` (id, username, password, auth_mode, firstname, lastname, email) VALUES (99, 'demo', '$2a$11$D2UTH3CVOU91KgRus8VMW.396gUE/L3JDKxDSK7cxWuEpmqUuoxEu	', 'DATABASE', 'Demo', 'Suricate', 'mvincent.it@gmail.com');
-INSERT INTO `user_role` (user_id, role_id) VALUES (99, 1);
+INSERT IGNORE INTO `role` (id, name, description) VALUES(1, 'ROLE_ADMIN', 'Administrator Role');
+INSERT IGNORE INTO `role` (id, name, description) VALUES(2, 'ROLE_USER', 'User role');
+INSERT IGNORE INTO `user` (id, username, password, auth_mode, firstname, lastname, email) VALUES (99, 'demo', '$2a$11$D2UTH3CVOU91KgRus8VMW.396gUE/L3JDKxDSK7cxWuEpmqUuoxEu	', 'DATABASE', 'Demo', 'Suricate', 'mvincent.it@gmail.com');
+INSERT IGNORE INTO `user_role` (user_id, role_id) VALUES (99, 1);
