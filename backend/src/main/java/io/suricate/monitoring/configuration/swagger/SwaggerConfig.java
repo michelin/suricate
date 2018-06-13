b4fd6993333052ffb1eafb3e4948f57cf22f8219
milan kamboya <kamboyamilan@gmail.com>
2018-06-13 22:20:27 +0530

    Issue #93 : added restriction for swagger documantion for dev profile only

--- backend/src/main/java/io/suricate/monitoring/configuration/swagger/SwaggerConfig.java
+++ backend/src/main/java/io/suricate/monitoring/configuration/swagger/SwaggerConfig.java
@@ -19,6 +19,8 @@ package io.suricate.monitoring.configuration.swagger;
 import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -30,6 +32,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import static com.google.common.base.Predicates.or;
 import static springfox.documentation.builders.PathSelectors.regex;
 
+@Profile("dev")
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
