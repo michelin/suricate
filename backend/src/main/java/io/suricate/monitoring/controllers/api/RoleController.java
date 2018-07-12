2f91639b3e80c6db41906a3d510ea97a3d260780
Axel Chesné <axel.chesnes_ext@michelin.com>
2018-07-12 11:48:38 +0200

    Issue #291 - Swagger documentation for RoleController

--- backend/src/main/java/io/suricate/monitoring/controllers/api/RoleController.java
+++ backend/src/main/java/io/suricate/monitoring/controllers/api/RoleController.java
@@ -16,12 +16,16 @@
 
 package io.suricate.monitoring.controllers.api;
 
+import io.suricate.monitoring.model.dto.error.ApiErrorDto;
 import io.suricate.monitoring.model.dto.user.RoleDto;
 import io.suricate.monitoring.model.entity.user.Role;
 import io.suricate.monitoring.model.mapper.role.RoleMapper;
 import io.suricate.monitoring.service.api.RoleService;
 import io.suricate.monitoring.utils.exception.NoContentException;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -77,6 +81,13 @@ public class RoleController {
      *
      * @return The list of roles
      */
+    @ApiOperation(value = "Add a user to a project", response = RoleDto.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Ok", response = RoleDto.class),
+        @ApiResponse(code = 204, message = "No Content"),
+        @ApiResponse(code = 401, message = "Authentication error, token expired or invalid", response = ApiErrorDto.class),
+        @ApiResponse(code = 403, message = "You don't have permission to access to this resource", response = ApiErrorDto.class),
+    })
     @RequestMapping(method = RequestMethod.GET)
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<List<RoleDto>> getRoles() {
