48b75339cba2702918a83e35e73d569baef06b72
Axel Chesné <axel.chesnes_ext@michelin.com>
2018-07-12 16:27:48 +0200

    Issue #291 - Swagger documentation for ProjectWidgetDto

--- backend/src/main/java/io/suricate/monitoring/model/dto/project/ProjectWidgetDto.java
+++ backend/src/main/java/io/suricate/monitoring/model/dto/project/ProjectWidgetDto.java
@@ -22,6 +22,8 @@ package io.suricate.monitoring.model.dto.project;
 import io.suricate.monitoring.model.dto.AbstractDto;
 import io.suricate.monitoring.model.dto.widget.WidgetDto;
 import io.suricate.monitoring.model.enums.WidgetState;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import java.util.Date;
@@ -31,66 +33,83 @@ import java.util.Date;
  * This is the instantiation of a widget
  * Link a widget with a dashboard/project
  */
-@Getter @Setter @NoArgsConstructor @EqualsAndHashCode(callSuper = false) @ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@ToString
+@ApiModel(value = "ProjectWidget", description = "Describe an instantiation of a widget")
 public class ProjectWidgetDto extends AbstractDto {
 
     /**
      * The project widget id
      */
+    @ApiModelProperty(value = "The project widget id")
     private Long id;
 
     /**
      * The data of the last execution
      */
+    @ApiModelProperty(value = "The data of the last execution of this widget")
     private String data;
 
     /**
      * The position of this instance of widget in the grid
      */
+    @ApiModelProperty(value = "The position of the widget on the grid")
     private ProjectWidgetPositionDto widgetPosition;
 
     /**
      * The css style for this instance
      */
+    @ApiModelProperty(value = "The css for this instance of widget")
     private String customStyle;
 
     /**
      * The instantiation of the html widget template
      */
+    @ApiModelProperty(value = "The html of the widget instantiate with the params")
     private String instantiateHtml;
 
     /**
      * Contains the configuration of the widget
      */
+    @ApiModelProperty(value = "The configuration of this widget")
     private String backendConfig;
 
     /**
      * The log of the last nashorn execution
      */
+    @ApiModelProperty(value = "The log of the execution")
     private String log;
 
     /**
      * The date of the last execution
      */
+    @ApiModelProperty(value = "The last execution date")
     private Date lastExecutionDate;
 
     /**
      * The date of the last execution success of nashorn
      */
+    @ApiModelProperty(value = "The last successful execution date")
     private Date lastSuccessDate;
 
     /**
      * The widget state {@link WidgetState}
      */
+    @ApiModelProperty(value = "The current widget state")
     private WidgetState state;
 
     /**
      * The related project
      */
+    @ApiModelProperty(value = "The related project")
     private ProjectDto project;
 
     /**
      * The related widget
      */
+    @ApiModelProperty(value = "The related widget")
     private WidgetDto widget;
 }
