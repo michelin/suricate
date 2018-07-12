bdf692619096e3df293afeb00ffeb68adbc8a996
Axel Chesné <axel.chesnes_ext@michelin.com>
2018-07-12 17:14:19 +0200

    Issue #291 - Swagger documentation for UserDto

--- backend/src/main/java/io/suricate/monitoring/model/dto/user/UserDto.java
+++ backend/src/main/java/io/suricate/monitoring/model/dto/user/UserDto.java
@@ -19,6 +19,8 @@ package io.suricate.monitoring.model.dto.user;
 import io.suricate.monitoring.model.dto.AbstractDto;
 import io.suricate.monitoring.model.dto.setting.UserSettingDto;
 import io.suricate.monitoring.model.enums.AuthenticationMethod;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import java.util.ArrayList;
@@ -32,60 +34,72 @@ import java.util.List;
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 @ToString
+@ApiModel(value = "User", description = "Describe a user of the app")
 public class UserDto extends AbstractDto {
 
     /**
-     * Data base id
+     * Database id
      */
+    @ApiModelProperty(value = "The id of the user")
     private Long id;
 
     /**
      * User firstname
      */
+    @ApiModelProperty(value = "The firstname of the user")
     private String firstname;
 
     /**
      * User lastname
      */
+    @ApiModelProperty(value = "The lastname of the user")
     private String lastname;
 
     /**
      * User fullname
      */
+    @ApiModelProperty(value = "The fullname of the user")
     private String fullname;
 
     /**
      * username
      */
+    @ApiModelProperty(value = "The username used as login in the credentials")
     private String username;
 
     /**
      * Mail
      */
+    @ApiModelProperty(value = "The user email")
     private String email;
 
     /**
      * Password of the user
      */
+    @ApiModelProperty(value = "The user password")
     private String password;
 
     /**
      * The confirmation password
      */
+    @ApiModelProperty(value = "The user password confirmation")
     private String confirmPassword;
 
     /**
      * The authentication method
      */
+    @ApiModelProperty(value = "The authentication method for this user")
     private AuthenticationMethod authenticationMethod;
 
     /**
      * User roles
      */
+    @ApiModelProperty(value = "The list of related roles", dataType = "List")
     private List<RoleDto> roles = new ArrayList<>();
 
     /**
      * User settings
      */
+    @ApiModelProperty(value = "The user setting", dataType = "List")
     private List<UserSettingDto> userSettings = new ArrayList<>();
 }
