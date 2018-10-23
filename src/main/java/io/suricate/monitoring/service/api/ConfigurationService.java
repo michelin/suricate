5cd21d6b68d2cb4ab6f7fe46e1167157d721be38
Mat V <mvincent.it@gmail.com>
2018-10-23 08:52:41 +0200

    Add a generic profil based on env vars

--- src/main/java/io/suricate/monitoring/service/api/ConfigurationService.java
+++ src/main/java/io/suricate/monitoring/service/api/ConfigurationService.java
@@ -29,7 +29,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
