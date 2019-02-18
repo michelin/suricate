a36265fc87133df6e6c5e9d56e65d78307377e17
Axel Chesné <axel.chesnes_ext@michelin.com>
2019-02-18 09:12:57 +0100

    #448 - Modify input generic component

--- src/main/webapp/app/shared/components/form/inputs/file-input/file-input.component.ts
+++ src/main/webapp/app/shared/components/form/inputs/file-input/file-input.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {InputComponent} from '../input.component';
 
 @Component({
   selector: 'app-file-input',
   templateUrl: './file-input.component.html',
   styleUrls: ['./file-input.component.scss']
 })
-export class FileInputComponent implements OnInit {
+export class FileInputComponent extends InputComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    super();
   }
 
+  /**
+   * Get the upload file as base 64
+   *
+   * @param event
+   * @param {NgForm} formSettings
+   * @param {string} inputName
+   * @param {string} regexValidator
+   */
+  getUploadedFileBase64(event: any, formSettings: NgForm, inputName: string, regexValidator: string) {
+    const fileReader = new FileReader();
+    const regexValidation = new RegExp(regexValidator, 'g');
+
+    if (event.target.files && event.target.files.length > 0) {
+      const file: File = event.target.files[0];
+
+      if (regexValidation.test(file.name)) {
+        fileReader.readAsDataURL(file);
+        fileReader.onloadend = () => {
+          formSettings.form.get(inputName).setValue(fileReader.result);
+        };
+
+        document.getElementsByClassName(`file-selection-sentence-${inputName}`)[0].textContent = file.name;
+      }
+    }
+  }
 }
--- src/main/webapp/app/shared/components/form/inputs/input.component.ts
+++ src/main/webapp/app/shared/components/form/inputs/input.component.ts
@@ -1,6 +1,26 @@
+/*
+ *  /*
+ *  * Copyright 2012-2018 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormGroup, ValidatorFn, Validators} from '@angular/forms';
+
 import {DataType} from '../../../model/enums/DataType';
+import {FormField} from '../../../model/app/form/FormField';
 
 /**
  * Manage the instantiation of different form inputs
@@ -16,25 +36,19 @@ export class InputComponent {
    * The form created in which we have to create the input
    */
   @Input()
-  form: FormGroup;
+  formGroup: FormGroup;
 
   /**
    * Object that hold different information used for the instantiation of the input
    */
   @Input()
-  field: any;
+  field: FormField;
 
   /**
    * True if the field should be readonly
    */
   @Input()
   isReadOnly: boolean;
-
-  /**
-   * An identifier for the field
-   */
-  @Input()
-  identifier: any;
 
   /**
    * Event sent when the value of the input has changed
@@ -47,30 +61,40 @@ export class InputComponent {
    */
   dataType = DataType;
 
+  /**
+   * Constructor
+   */
   constructor() {
   }
 
   /**
    * Retrieve the form control from the form
    */
-  getFormControl() {
-    return this.form.get(this.field.key);
+  getFormControl(): AbstractControl | null {
+    return this.formGroup.controls[this.field.key];
   }
 
   /**
    * Function called when a field has been changed in the form, emit and event that will be caught by the parent component
    *
-   * @param id The id of the field
    * @param value The new value
    */
-  emitValueChange(id, value) {
-    this.valueChangeEvent.emit({identifier: id, value: value});
+  emitValueChange(value): void {
+    this.valueChangeEvent.emit({value: value});
   }
 
   /**
    * Tell if it's a required field
    */
-  isRequired() {
-    return !this.isReadOnly && this.field && this.field.validators && this.field.validators.includes(Validators.required);
+  isRequired(): boolean {
+    let isRequired: boolean = false;
+
+    if (!this.isReadOnly && this.field && this.field.validators && this.field.validators) {
+      isRequired = Array.isArray(this.field.validators) ?
+        (this.field.validators as ValidatorFn[]).includes(Validators.required)
+        : this.field.validators === Validators.required;
+    }
+
+    return isRequired;
   }
 }
