7f284357b152489ab0114c6751739f78a2a173fa
Axel Chesné <axel.chesnes_ext@michelin.com>
2019-02-21 18:23:20 +0100

    #448 - delete uploaded file function

--- src/main/webapp/app/modules/dashboard/components/edit-project-widget-dialog/edit-project-widget-dialog.component.ts
+++ src/main/webapp/app/modules/dashboard/components/edit-project-widget-dialog/edit-project-widget-dialog.component.ts
@@ -223,32 +223,6 @@ export class EditProjectWidgetDialogComponent implements OnInit {
   }
 
   /**
-   * Get the upload file as base 64
-   *
-   * @param event
-   * @param {NgForm} formSettings
-   * @param {string} inputName
-   * @param {string} regexValidator
-   */
-  getUploadedFileBase64(event: any, formSettings: NgForm, inputName: string, regexValidator: string) {
-    const fileReader = new FileReader();
-    const regexValidation = new RegExp(regexValidator, 'g');
-
-    if (event.target.files && event.target.files.length > 0) {
-      const file: File = event.target.files[0];
-
-      if (regexValidation.test(file.name)) {
-        fileReader.readAsDataURL(file);
-        fileReader.onloadend = () => {
-          formSettings.form.get(inputName).setValue(fileReader.result);
-        };
-
-        document.getElementsByClassName(`file-selection-sentence-${inputName}`)[0].textContent = file.name;
-      }
-    }
-  }
-
-  /**
    * Get the param value inside the backend config for a param
    *
    * @param {string} config The backend config
