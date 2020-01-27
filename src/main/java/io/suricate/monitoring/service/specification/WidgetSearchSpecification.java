9e259c94c37f70281010f29890e21fd2fb6ff41f
Loïc Greffier <loic.greffier_ext@michelin.com>
2020-01-27 10:50:20 +0100

    Add widgets research by category name in the catalog

--- src/main/java/io/suricate/monitoring/service/specification/WidgetSearchSpecification.java
+++ src/main/java/io/suricate/monitoring/service/specification/WidgetSearchSpecification.java
@@ -1,7 +1,13 @@
 package io.suricate.monitoring.service.specification;
 
+import io.suricate.monitoring.model.entity.widget.Category;
+import io.suricate.monitoring.model.entity.widget.Category_;
 import io.suricate.monitoring.model.entity.widget.Widget;
 import io.suricate.monitoring.model.entity.widget.Widget_;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
 
 
 /**
@@ -16,5 +22,22 @@ public class WidgetSearchSpecification extends AbstractSearchSpecification<Widge
      */
     public WidgetSearchSpecification(final String search) {
         super(search, Widget_.name);
+    }
+
+    /**
+     * Search widgets by category name
+     *
+     * @param categoryName  The name of the category to search by
+     * @return A widget specification
+     */
+    public static Specification<Widget> getWidgetByCategoryNameSpecification(final String categoryName) {
+        String categoryNameFilter = categoryName != null ? "%" + categoryName.toLowerCase() + "%" : null;
+
+        return (root, query, criteriaBuilder) -> {
+            Join<Widget, Category> join = root.join(Widget_.category);
+
+            return criteriaBuilder.like(
+                    criteriaBuilder.lower(join.get(Category_.name)), categoryNameFilter);
+        };
     }
 }
