8b5795fbafbf278f640c53c0a104fffd27018234
Loïc GREFFIER <loic.greffier@hotmail.fr>
2022-06-25 03:35:54 +0200

    Fix ng unit tests

--- src/main/webapp/app/core/components/settings/ux-settings/ux-settings.component.spec.ts
+++ src/main/webapp/app/core/components/settings/ux-settings/ux-settings.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UxSettingsComponent } from './ux-settings.component';
 import { MockModule } from '../../../../mock/mock.module';
+import { AuthenticationService } from '../../../../shared/services/frontend/authentication/authentication.service';
 
 describe('UxSettingsComponent', () => {
   let component: UxSettingsComponent;
@@ -12,6 +13,10 @@ describe('UxSettingsComponent', () => {
       imports: [MockModule],
       declarations: [UxSettingsComponent]
     }).compileComponents();
+
+    AuthenticationService.setAccessToken(
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdG5hbWUiOiJMb2ljIiwibWFpbCI6ImxvaWMuZ3JlZmZpZXJfZXh0QG1pY2hlbGluLmNvbSIsInVzZXJfbmFtZSI6ImZ4MzA2MzIiLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXSwiZXhwIjoxNjAxNjUzMjQzLCJhdXRob3JpdGllcyI6WyJST0xFX0FETUlOIl0sImp0aSI6IjgxNWY4M2VmLWYxMDktNDQ4My1hZGYzLTRhNDBkNzBlMzc5YSIsImNsaWVudF9pZCI6InN1cmljYXRlQW5ndWxhciIsImxhc3RuYW1lIjoiR3JlZmZpZXIifQ.B6C4CckHoOoDZi83dPJaxJZ7-SPaIwT7FNUgwGiKLm0'
+    );
   });
 
   beforeEach(() => {
