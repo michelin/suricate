46779e6c6d8bc042ba6f02392144573bce7caae1
Loïc Greffier <loic.greffier_ext@michelin.com>
2023-02-27 16:03:06 +0100

    Improved postgres testcontainers

--- src/test/java/com/michelin/suricate/integrations/PostgreSQLIntegrationTest.java
+++ src/test/java/com/michelin/suricate/integrations/PostgreSQLIntegrationTest.java
@@ -18,7 +18,13 @@
 
 package com.michelin.suricate.integrations;
 
+import com.michelin.suricate.model.entities.AllowedSettingValue;
+import com.michelin.suricate.model.entities.Role;
 import com.michelin.suricate.model.entities.Setting;
+import com.michelin.suricate.model.enums.DataTypeEnum;
+import com.michelin.suricate.model.enums.SettingType;
+import com.michelin.suricate.repositories.AllowedSettingValueRepository;
+import com.michelin.suricate.repositories.RoleRepository;
 import com.michelin.suricate.repositories.SettingRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +36,7 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -39,9 +46,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("integration-test-postgresql")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PostgreSQLIntegrationTest {
-
     @Autowired
     private SettingRepository settingRepository;
+
+    @Autowired
+    private AllowedSettingValueRepository allowedSettingValueRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Container
     public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres")
@@ -57,10 +69,63 @@ class PostgreSQLIntegrationTest {
     }
 
     @Test
-    void shouldFlywayInitSchemaOnPostgreSQL() {
+    void shouldContainsSettings() {
         Optional<List<Setting>> actual = settingRepository.findAllByOrderByDescription();
 
         assertThat(actual).isPresent();
         assertThat(actual.get()).hasSize(2);
+        assertThat(actual.get().get(0).isConstrained()).isTrue();
+        assertThat(actual.get().get(0).getDataType()).isEqualTo(DataTypeEnum.COMBO);
+        assertThat(actual.get().get(0).getType()).isEqualTo(SettingType.THEME);
+        assertThat(actual.get().get(0).getDescription()).isEqualTo("Theme");
+
+        assertThat(actual.get().get(1).isConstrained()).isTrue();
+        assertThat(actual.get().get(1).getDataType()).isEqualTo(DataTypeEnum.COMBO);
+        assertThat(actual.get().get(1).getType()).isEqualTo(SettingType.LANGUAGE);
+        assertThat(actual.get().get(1).getDescription()).isEqualTo("Language");
+    }
+
+    @Test
+    void shouldContainsAllowedSettingValues() {
+        Optional<List<Setting>> actual = settingRepository.findAllByOrderByDescription();
+
+        List<AllowedSettingValue> actualAllowedSettingValues = new ArrayList<>();
+        allowedSettingValueRepository.findAll().forEach(actualAllowedSettingValues::add);
+
+        assertThat(actual).isPresent();
+
+        assertThat(actualAllowedSettingValues).hasSize(4);
+        assertThat(actualAllowedSettingValues.get(0).getTitle()).isEqualTo("Default");
+        assertThat(actualAllowedSettingValues.get(0).getValue()).isEqualTo("default-theme");
+        assertThat(actualAllowedSettingValues.get(0).isDefault()).isTrue();
+        assertThat(actualAllowedSettingValues.get(0).getSetting()).isEqualTo(actual.get().get(0));
+
+        assertThat(actualAllowedSettingValues.get(1).getTitle()).isEqualTo("Dark");
+        assertThat(actualAllowedSettingValues.get(1).getValue()).isEqualTo("dark-theme");
+        assertThat(actualAllowedSettingValues.get(1).isDefault()).isFalse();
+        assertThat(actualAllowedSettingValues.get(1).getSetting()).isEqualTo(actual.get().get(0));
+
+        assertThat(actualAllowedSettingValues.get(2).getTitle()).isEqualTo("English");
+        assertThat(actualAllowedSettingValues.get(2).getValue()).isEqualTo("en");
+        assertThat(actualAllowedSettingValues.get(2).isDefault()).isTrue();
+        assertThat(actualAllowedSettingValues.get(2).getSetting()).isEqualTo(actual.get().get(1));
+
+        assertThat(actualAllowedSettingValues.get(3).getTitle()).isEqualTo("Français");
+        assertThat(actualAllowedSettingValues.get(3).getValue()).isEqualTo("fr");
+        assertThat(actualAllowedSettingValues.get(3).isDefault()).isFalse();
+        assertThat(actualAllowedSettingValues.get(3).getSetting()).isEqualTo(actual.get().get(1));
+    }
+
+    @Test
+    void shouldContainsRoles() {
+        List<Role> actual = new ArrayList<>();
+        roleRepository.findAll().forEach(actual::add);
+
+        assertThat(actual).hasSize(2);
+        assertThat(actual.get(0).getDescription()).isEqualTo("Administrator Role");
+        assertThat(actual.get(0).getName()).isEqualTo("ROLE_ADMIN");
+        
+        assertThat(actual.get(1).getDescription()).isEqualTo("User role");
+        assertThat(actual.get(1).getName()).isEqualTo("ROLE_USER");
     }
 }
