adfb90da763872861feb595378e4c1069eec45e9
Loïc Greffier <loic.greffier@hotmail.fr>
2023-11-12 12:53:06 +0100

    update angular core 12

--- src/main/webapp/polyfills.ts
+++ src/main/webapp/polyfills.ts
@@ -33,7 +33,7 @@ import 'web-animations-js'; // Run `npm install --save web-animations-js`.
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone'; // Included with Angular CLI.
+import 'zone.js'; // Included with Angular CLI.
 /** IE11 polyfill */
 import 'core-js/es7/array';
 /** IE11 requires to enable URLSearchParams. */
