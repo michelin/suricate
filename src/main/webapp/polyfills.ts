06169d73333a58e59a5d6786b44180ce4f6c66d9
Loïc Greffier <loic.greffier@hotmail.fr>
2023-12-03 17:16:25 +0100

    multiple fixes

--- src/main/webapp/polyfills.ts
+++ src/main/webapp/polyfills.ts
@@ -14,45 +14,4 @@
  * Learn more in https://angular.io/guide/browser-support
  */
 
-/***************************************************************************************************
- * BROWSER POLYFILLS
- */ // Run `npm install --save classlist.js`.
-/** IE10 and IE11 requires the following for the Reflect API. */
-import 'core-js/es6/reflect';
-/** Evergreen browsers require these. **/
-// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es7/reflect'; // Run `npm install --save web-animations-js`.
-/***************************************************************************************************
- * Zone JS is required by default for Angular itself.
- */
-import 'zone.js'; // Included with Angular CLI.
-/** IE11 polyfill */
-import 'core-js/es7/array';
-/** IE11 requires to enable URLSearchParams. */
-import 'url-search-params-polyfill'; // Run `npm install --save url-search-params-polyfill`
-
-/**
- * By default, zone.js will patch all possible macroTask and DomEvents
- * user can disable parts of macroTask/DomEvents patch by setting following flags
- */
-
-// (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
-// (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
-// (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
 (window as any).global = window;
-
-/*
- * in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
- * with the following flag, it will bypass `zone.js` patch for IE/Edge
- */
-// (window as any).__Zone_enable_cross_context_check = true;
-
-/***************************************************************************************************
- * APPLICATION IMPORTS
- */
-
-/**
- * Date, currency, decimal and percent pipes.
- * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
- */
-// Run `npm install --save intl`.
