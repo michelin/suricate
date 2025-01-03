b5e78986c1d64541552ff4812ae12c438af8d6ce
Loïc GREFFIER <loic.greffier@michelin.com>
2025-01-03 11:09:21 +0100

    Fix probe content type detection on Ubuntu 24.04 LTS (#921)
    * Fix pull request
    
    * Fix pull request
    
    * Fix pull request

--- src/test/java/com/michelin/suricate/util/FilesUtilsTest.java
+++ src/test/java/com/michelin/suricate/util/FilesUtilsTest.java
@@ -1,6 +1,8 @@
 package com.michelin.suricate.util;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.michelin.suricate.model.entity.Asset;
 import java.io.File;
@@ -14,57 +16,58 @@ class FilesUtilsTest {
     void shouldGetFolders() throws IOException {
         List<File> actual = FilesUtils.getFolders(new File("src/test/resources/repository"));
 
-        assertThat(actual).hasSize(2);
-        assertThat(actual.get(0).getName()).contains("content");
-        assertThat(actual.get(1).getName()).contains("libraries");
+        assertEquals(2, actual.size());
+        assertTrue(actual.get(0).getName().contains("content"));
+        assertTrue(actual.get(1).getName().contains("libraries"));
     }
 
     @Test
     void shouldGetNoFolder() throws IOException {
         List<File> actual = FilesUtils.getFolders(null);
 
-        assertThat(actual).isEmpty();
+        assertTrue(actual.isEmpty());
     }
 
     @Test
     void shouldGetFiles() throws IOException {
         List<File> actual = FilesUtils.getFiles(new File("src/test/resources/repository/libraries"));
 
-        assertThat(actual).hasSize(1);
-        assertThat(actual.get(0).getName()).contains("test.js");
+        assertEquals(1, actual.size());
+        assertTrue(actual.getFirst().getName().contains("test.js"));
     }
 
     @Test
     void shouldGetNoFile() throws IOException {
         List<File> actual = FilesUtils.getFiles(null);
 
-        assertThat(actual).isEmpty();
+        assertTrue(actual.isEmpty());
     }
 
     @Test
     void shouldReadJsAsset() throws IOException {
         Asset actual = FilesUtils.readAsset(new File("src/test/resources/repository/libraries/test.js"));
 
-        assertThat(actual).isNotNull();
-        assertThat(actual.getContentType()).isEqualTo("application/javascript");
+        assertNotNull(actual);
+        assertEquals("application/javascript", actual.getContentType());
     }
 
     @Test
     void shouldReadImageAsset() throws IOException {
         Asset actual = FilesUtils.readAsset(
-            new File("src/test/resources/repository/content/github/widgets/count-issues/image.png"));
+            new File("src/test/resources/repository/content/github/widgets/count-issues/image.png")
+        );
 
-        assertThat(actual).isNotNull();
-        assertThat(actual.getContentType()).isEqualTo("image/png");
+        assertNotNull(actual);
+        assertEquals("image/png", actual.getContentType());
     }
 
     @Test
     void shouldSetContentTypeToDefaultTextPlain() throws IOException {
-        Asset actual =
-            FilesUtils.readAsset(new File("src/test/resources/repository/content/other/description.yml"));
+        Asset actual = FilesUtils.readAsset(new File("src/test/resources/repository/content/other/description.yml"));
 
-        assertThat(actual.getContentType()).isEqualTo("text/plain");
-        assertThat(actual.getSize()).isPositive();
-        assertThat(actual.getContent()).isNotEmpty();
+        // Ubuntu 24.04 LTS returns "application/yaml"
+        assertTrue(List.of("text/plain", "application/yaml").contains(actual.getContentType()));
+        assertTrue(actual.getSize() > 0);
+        assertTrue(actual.getContent().length > 0);
     }
 }
