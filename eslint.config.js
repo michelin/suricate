5602f8a140e25a477327c9c9cf1625f8501764d7
Loïc Greffier <loic.greffier@michelin.com>
2025-01-07 23:08:59 +0100

    Fix prettier and eslint

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const unusedImports = require('eslint-plugin-unused-imports');
+const deprecation = require('eslint-plugin-deprecation');
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+    plugins: {
+      "unused-imports": unusedImports,
+      "deprecation": deprecation,
+    },
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      eslintPluginPrettierRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "unused-imports/no-unused-imports": 'error',
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "suricate",
+          style: "kebab-case",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+);
