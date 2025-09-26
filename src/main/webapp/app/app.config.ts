b8fff1cee0bea1db7bd84c0c8bf42799bb389a5b
Loïc Greffier <loic.greffier@michelin.com>
2025-09-27 01:45:39 +0200

    Migrate token interceptor

--- src/main/webapp/app/app.config.ts
+++ src/main/webapp/app/app.config.ts
@@ -1,5 +1,5 @@
 import { DatePipe } from '@angular/common';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideTranslateService } from '@ngx-translate/core';
@@ -7,15 +7,14 @@ import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { appRoutes } from './app.routes';
 import { ErrorInterceptor } from './shared/interceptors/error-interceptor';
-import { TokenInterceptor } from './shared/interceptors/token-interceptor';
+import { tokenInterceptor } from './shared/interceptors/token-interceptor';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideBrowserGlobalErrorListeners(),
 		provideZoneChangeDetection({ eventCoalescing: true }),
-		provideHttpClient(withInterceptorsFromDi()),
+		provideHttpClient(withInterceptors([tokenInterceptor])),
 		provideRouter(appRoutes),
-		{ provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
 		{ provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
 		provideTranslateService({
 			loader: provideTranslateHttpLoader({ prefix: './assets/i18n/', suffix: '.json' })
